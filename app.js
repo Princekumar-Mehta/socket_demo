2a3b35b1bf1cb804b678329f79c4ead42f4d1deb
Princekumar-Mehta <2001.mehta.2001@gmail.com>
2023-05-01 13:30:03 +0530

    channel or rooms with limit

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
-import  express  from "express";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import http from "http"
-import { Server } from 'socket.io';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import http from "http";
+import { Server } from "socket.io";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const PORT = 8000;
 
 const server = http.createServer(app);
-const socketio =new Server(server);
-app.get("/",(request,response)=>{
-    const options = {
-        root:path.join(__dirname)
-    }
-    const filename = "index.html";
-  response.sendFile(filename,options)
-})
-
-const cnsp = socketio.of("/custom-namespace");
-cnsp.on("connection",(socket)=>{
-    console.log("one user is connected");
-   cnsp.emit("customEvent","Tester event call")
-    cnsp.on("disconnect",()=>{
-        users--;
+const socketio = new Server(server);
+app.get("/", (request, response) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const filename = "index.html";
+  response.sendFile(filename, options);
+});
+let roomno = 0;
+let full = 0;
+socketio.on("connection", (socket) => {
+  console.log("one user is connected");
+  socket.join("room-" + roomno);
+  
+  socketio.sockets
+    .in("room-" + roomno)
+    .emit("connectedRoom", "You are connected to room no. " + roomno);
+    socket.on("disconnect",()=>{
         console.log("one user disconnected");
     })
-})
-const start = async ()=>{
-    try{
-        server.listen(PORT, ()=>{
-            console.log(PORT, "Hi I am listening");
-        })
-    }catch(err){
-        console.log(err);
-    }
-}
+    full++;
+  if (full >= 2) {
+    full = 0;
+    roomno++;
+  }
+});
+const start = async () => {
+  try {
+    server.listen(PORT, () => {
+      console.log(PORT, "Hi I am listening");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 start();
